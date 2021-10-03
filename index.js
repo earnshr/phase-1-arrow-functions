de1bcc6dba6da62f4da8f1cd92e28f137e1ad5b9
bexearn12 <earnshr@gmail.com>
2021-10-03 12:06:36 -0500

    commit tree 532c624199f74533119e5f47082b263ea7338eb8

---
+++ index.js
@@ -0,0 +1,41 @@
+function divide(num1, num2){
+    return 2000 / 100
+}
+
+const square = x => x**2
+
+const add = (a,b) => a + b
+let a = 3
+let b = 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //const add = (parameter1, parameter2) => parameter1 + parameter2;
+// const divide = (num1, num2) => 2000 / 100;
+
+// let x = 2
+// const square = (x, num3) => x**2;
+
+// let a = 3
+// let b = 4
+// const add = (a, b) => 3 + 4;
+
+
+
